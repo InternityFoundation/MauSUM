6d173896b032f192d2ba301225adb95b84bfd295
Mohit Khandelwal <mohitkh7@gmail.com>
2020-02-16 13:01:30 +0530

    old data

--- backend/weather.py
+++ backend/weather.py
@@ -4,6 +4,7 @@ import jsonpickle
 import sys
 import requests
 import time
+from flask_cors import CORS, cross_origin
 from uuid import uuid4
 from urllib.parse import urlparse
 from flask import Flask, jsonify, request, render_template
@@ -16,15 +17,23 @@ LATITUDE = 'latitude'
 LONGITUDE = 'longitude'
 
 class Weather:
-    def __init__(self, lat, lon, temp, hum):
+    def __init__(self, lat, lon, temp, hum, pre, con="", icon=""):
         self.latitude = lat
         self.longitude = lon
-        self.temperature = temp
-        self.humidity = hum
+        self.temperature = round(temp, 1)
+        self.humidity = round(hum, 1)
+        self.pressure = round(pre, 1)
+        self.condition = con
+        self.icon = icon
+        self.future = []
         self.time = time.time()
 
+    def set_future_data(self, data):
+        if data is not None:
+            self.future = data
+
     def __str__(self):
-        return str("Lat: %s Long: %s Temp: %s" %(self.latitude, self.longitude, self.temperature))
+        return str("Lat: %s Long: %s Temp: %s icon: %s" %(self.latitude, self.longitude, self.temperature, self.icon))
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -38,44 +47,65 @@ def calculate_weather(weather_obj_list):
     temp = 0
     hum = 0
     count = 0
+    pre = 0
+    icon = ""
+    con = ""
+    future = []
     for weather in weather_obj_list:
+        print(weather)
         if weather is None:
             continue
         lat = weather.latitude
         lon = weather.longitude
+        if weather.icon != "":
+            icon = weather.icon
+        if weather.condition != "":
+            con = weather.condition
+        if len(weather.future) > 0:
+            future = weather.future
         temp += weather.temperature
         hum += weather.humidity
+        pre += weather.pressure
         count += 1
     temp /= count
     hum /= count
+    pre /= count
 
-    cal_weather_obj = Weather(lat, lon, temp, hum)
+    cal_weather_obj = Weather(lat, lon, temp, hum, pre, con, icon)
+    cal_weather_obj.set_future_data(future)
     return cal_weather_obj
 
+def get_future_weather_darksky(lst):
+    if lst is None:
+        return []
+    ans = []
+    for data in lst:
+        time = data["time"]
+        temp = round((data["temperatureHigh"] + data["temperatureLow"]) / 2, 1)
+        dic = {"time": time, "temp": temp}
+        ans.append(dic)
+    return ans
+
 def get_current_darksky(lat, lon):
-    """
     api_key = keys['darksky']
     ENDPOINT = 'https://api.darksky.net/forecast/' + api_key + '/'
 
-    url = ENDPOINT + lat + ',' + lon
+    url = ENDPOINT + lat + ',' + lon +'?units=si'
     response = requests.get(url)
     if response.status_code == 200:
         data = json.loads(response.text)
         current_weather = data["currently"]
         temp = current_weather["temperature"]
+        temp = temperature.celsius_to_kelvin(temp)
         hum = current_weather["humidity"]
-        w_obj = Weather(lat, lon, temp, hum)
+        pre = current_weather["pressure"]
+        w_obj = Weather(lat, lon, temp, hum, pre)
+        future_data = get_future_weather_darksky(data["daily"]["data"]);
+        w_obj.set_future_data(future_data)
         return w_obj
     else:
         print("FAILED")
-    """
-    with open('../data/darksky.json') as f:
-        data = json.load(f)
-        current_weather = data["currently"]
-        temp = temperature.fahrenheit_to_kelvin(current_weather["temperature"])
-        hum = current_weather["humidity"]
-        w_obj = Weather(lat, lon, temp, hum)
-        return w_obj
+
 
 def get_current_openweather(lat, lon):
     api_key = keys['openweather']
@@ -90,13 +120,17 @@ def get_current_openweather(lat, lon):
         current_weather = data.get("main")
         temp = current_weather["temp"]
         hum = current_weather["humidity"]
-        w_obj = Weather(lat, lon, temp, hum)
+        pre = current_weather["pressure"]
+        con = data.get("weather")[0].get("description")
+        icon = data.get("weather")[0].get("icon")
+        w_obj = Weather(lat, lon, temp, hum, pre, con, icon)
         return w_obj
     else:
         print(response.text)
         print("FAILED")
 
 app = Flask(__name__, static_url_path='')
+cors = CORS(app)
 
 @app.route('/weather', methods=['GET'])
 def get_current_weather():
@@ -111,11 +145,9 @@ def get_current_weather():
     weather_obj_list.append(get_current_darksky(lat, lon))
     weather_obj_list.append(get_current_openweather(lat, lon))
     weather = calculate_weather(weather_obj_list)
-    response = {
-        'weather': jsonpickle.encode(weather, unpicklable=False)
-    }
+    response = jsonpickle.encode(weather, unpicklable=False)
 
-    return jsonify(response), 200
+    return response, 200
 
 
 if __name__ == '__main__':
